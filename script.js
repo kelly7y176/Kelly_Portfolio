c3b8fb934dddd7e913d7ecfee15e10d4151ae4d6
Kelly Wong <125940090+kelly7y176@users.noreply.github.com>
2024-07-26 02:16:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+
+
+
 var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
